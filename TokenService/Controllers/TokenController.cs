3b0954e81bd5c7c9ae9f4f873ba7a71c9f4106a2
Hans Cobian <c-hans.cobian@cart.com>
2022-06-13 15:18:16 -0500

    token service

---
+++ TokenService/Controllers/TokenController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace TokenService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration Configuration;
+        public TokenController(IConfiguration configuration)
+        {
+            this.Configuration = configuration; 
+        }
+
+        [HttpPost(Name = "Token")]
+        public IActionResult Post()
+        {
+            string body = string.Empty;
+            try
+            {
+                using (StreamReader stream = new StreamReader(Request.Body))
+                {
+                    body = stream.ReadToEnd();
+                }
+            }
+            catch (Exception ex) { }
+            Console.WriteLine($"payload: {body}");
+            var token = GenerateJWT(Environment.GetEnvironmentVariable("SB_JWT_CLIENT_KEY"), Configuration["SB_Jwt_Client:Issuer"], Configuration["SB_Jwt_Client:Audience"]);
+            return Ok(new { access_token = token, token_type  = "Bearer" });
+        }
+
+        private static string GenerateJWT(string key, string issuer, string audience)
+        {
+            var token = new JwtSecurityToken
+                (
+                issuer: issuer,
+                audience: audience,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)), SecurityAlgorithms.HmacSha256)
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
